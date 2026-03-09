086d8867dd7387e226fde21fe0f6d8bc8b2bce80
loginkarun <loginkarun@gmail.com>
2026-03-09 20:27:32 +0530

    Auto commit: Scripts committed on 2026-03-09-14-57-30

---
+++ code/src/test/java/com/myproject/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+        // Test that Spring context loads successfully
+    }
+}
